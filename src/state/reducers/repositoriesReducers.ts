78dec97f1704a6b77d5e48f9b9aff44eef8d95a0
Tiril Fjell <tirilfjell@Tirils-MacBook-Pro.local>
2021-09-15 10:56:41 +0200

    set up

---
+++ src/state/reducers/repositoriesReducers.ts
@@ -0,0 +1,11 @@
+interface RepositoriesState {
+    loading: boolean;
+    error: string | null;
+    data: string[]; 
+}
+
+const reducer = (state: RepositoriesState, action: any) => {
+
+}
+
+export default reducer;
