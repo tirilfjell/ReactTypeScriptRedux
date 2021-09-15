6f3c1ae779de23125735d3672784df4b7b2c65df
Tiril Fjell <tirilfjell@Tirils-MacBook-Pro.local>
2021-09-15 11:31:11 +0200

    new switch

--- src/state/reducers/repositoriesReducers.ts
+++ src/state/reducers/repositoriesReducers.ts
@@ -1,11 +1,38 @@
 interface RepositoriesState {
-    loading: boolean;
-    error: string | null;
-    data: string[]; 
+  loading: boolean;
+  error: string | null;
+  data: string[];
 }
 
-const reducer = (state: RepositoriesState, action: any) => {
-
+interface SerachRepositoriesAction {
+  type: "search_repositories";
 }
+interface SerachRepositoriesSuccessAction {
+  type: "search_repositories_success";
+  payload: string[];
+}
+interface SerachRepositoriesErrorAction {
+  type: "search_repositories_ error";
+  payload: string;
+}
+
+const reducer = (
+  state: RepositoriesState,
+  action:
+    | SerachRepositoriesAction
+    | SerachRepositoriesSuccessAction
+    | SerachRepositoriesErrorAction
+): RepositoriesState => {
+  switch (action.type) {
+    case "search_repositories":
+      return { loading: true, error: null, data: [] };
+    case "search_repositories_success":
+      return { loading: false, error: null, data: action.payload };
+    case "search_repositories_ error":
+      return { loading: false, error: action.payload, data: [] };
+    default:
+      return state;
+  }
+};
 
 export default reducer;
